87793cb1b06422e985277acaa356512eb9d6aac4
Tzwcard <tzwcard@gmail.com>
2024-12-09 05:19:47 +0800

    Add comments for export functions

--- iidx-submon/iidx-submon.cpp
+++ iidx-submon/iidx-submon.cpp
@@ -6,6 +6,10 @@
 
 static std::thread _th_gui;
 
+/*
+ * Init submon module
+ * return 1 on success, return 0 or less if failed
+ */
 int iidx_submon_init(void) {
 	/*
 	   Because we can only create one D3DDevice at a time
@@ -30,22 +34,42 @@ int iidx_submon_init(void) {
 	return 1;
 }
 
+/*
+ * Get turntable resistance value for player #p (0, 1)
+ * return corresponding resistance set from gui (0 - 255)
+ */
 unsigned char iidx_submon_get_resist(char p) {
 	return iidx_sub_gui_get_resist(p);
 }
 
+/*
+ * Get slider value for slider #id (0 - 4)
+ * return corresponding slider value set from gui (0 - 15)
+ */
 char iidx_submon_get_slider(char id) {
 	return iidx_sub_gui_get_slider(id);
 }
 
-bool iidx_submon_set_16seg(unsigned char* seg) {
-	return iidx_sub_gui_set_16seg(seg);
-}
-
+/*
+ * Get led area mask
+ * return mask value set from gui (0x00000 - 0x1FFFF)
+ */
 uint32_t iidx_submon_get_led_mask(void) {
 	return iidx_sub_gui_get_led_mask();
 }
 
+/*
+ * Set 16-segment string
+ * will do a memcpy of 9 unsigned char
+ * always return true
+ */
+bool iidx_submon_set_16seg(unsigned char* seg) {
+	return iidx_sub_gui_set_16seg(seg);
+}
+
+/*
+ * Finalize submon module
+ */
 void iidx_submon_finl(void) {
 	gui_close();
 	if (_th_gui.joinable()) {
